f499baf066f3b531a0ef3b03793b8be4cb8ddee6
Mike Stevens <mike@mwstevens.com>
2014-03-17 23:40:42 +0100

    SPNotify.Banner deployed and tested locally

---
+++ SPNotify.Banner/CONTROLTEMPLATES/SPNotify.Banner/Banner.ascx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+
+
+namespace SPNotify.Banner.CONTROLTEMPLATES.SPNotify.Banner
+{
+    public partial class Banner : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SPWebApplication spWebApp = SPContext.Current.Site.WebApplication;
+                if (spWebApp.Properties["BannerHtml"].ToString() != "")
+                {
+                    string strHTML = @"<style type=text/css>#Banner {" + spWebApp.Properties["BannerStyle"].ToString() + @" }</style>";
+                    litBannerStyle.Text = strHTML;
+                    litBannerHtml.Visible = true;
+
+                    strHTML = String.Format("<div id='Banner'>" + spWebApp.Properties["BannerHtml"].ToString() + "</div>");
+                    litBannerHtml.Text = strHTML;
+                }
+
+                //if (spWebApp.Properties["StatusAlertTitle"].ToString() != "")
+                //{
+
+                //    string strTitle = spWebApp.Properties["StatusAlertTitle"].ToString();
+                //    string strMessage = spWebApp.Properties["StatusAlertMessage"].ToString();
+                //    string strColor = spWebApp.Properties["StatusAlertcolor"].ToString().ToLower();
+
+                //    string strAlert = "<script type='text/javascript' language='javascript'>"
+                //        + "ExecuteOrDelayUntilScriptLoaded(AddCustomStatus, 'sp.js'); "
+                //        + "_spBodyOnLoadFunctionNames.push('AddCustomStatus'); "
+                //        + "function AddCustomStatus() { "
+                //        + "statusId = SP.UI.Status.addStatus(\'" + strTitle + "\', \'" + strMessage + "', true); "
+                //        + "SP.UI.Status.setStatusPriColor(statusId, \'" + strColor + "\'); return statusId; } "
+                //        + "</script>";
+
+                //    litStatusAlert.Text = strAlert;
+                //}
+            }
+            catch (Exception ex)
+            {
+                SPDiagnosticsService.Local.WriteTrace(0, new SPDiagnosticsCategory("Custom", TraceSeverity.Unexpected, EventSeverity.Error), TraceSeverity.Unexpected, ex.Message, ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ SPNotify.Banner/Features/SPNotify.Banner/SPNotify.Banner.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="dcf8c43c-33d7-46b2-ac28-2e64157792c7" description="SPWebApplication scoped banner for the top of every page. The style attributes and the content are set for each SPWebApplication in the property bag: [&quot;BannerHtml&quot;],[&quot;BannerStyle&quot;]" featureId="dcf8c43c-33d7-46b2-ac28-2e64157792c7" imageUrl="" scope="WebApplication" solutionId="00000000-0000-0000-0000-000000000000" title="SPNotify Banner" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="a36257fd-b46a-4084-a29a-22f70e1177f3" />
+  </projectItems>
+</feature>
